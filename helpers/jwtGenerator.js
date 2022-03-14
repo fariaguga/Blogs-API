4997a91df3ef38223d7ea0825122c8dde8a8cd11
Faria <fariaguga@hotmail.com>
2022-03-14 18:09:53 -0300

    fix: jwt

--- helpers/jwtGenerator.js
+++ helpers/jwtGenerator.js
@@ -4,6 +4,6 @@ const jwtConfig = {
   expiresIn: '1d',
 };
 
-const SECRET = process.env.JWT_SECRET;
+const SECRET = process.env.JWT_SECRET || 'Batatinha123';
 
 module.exports = (data = {}) => jwt.sign({ data }, SECRET, jwtConfig);
