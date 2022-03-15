497be621481aca8aa042db07bf78d1e33748f69e
Faria <fariaguga@hotmail.com>
2022-03-15 16:32:17 -0300

    fix: auth.middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -4,7 +4,7 @@ const auth = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
 
-    if (!token) return res.status(401).json({ error: 'Token not found' });
+    if (!token || token === '') return res.status(401).json({ error: 'Token not found' });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
