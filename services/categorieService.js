2ec1a2b68f78a603ab2b0d81595a61cda24045f7
Faria <fariaguga@hotmail.com>
2022-03-16 10:04:44 -0300

    feat: add getCategories service

--- services/categorieService.js
+++ services/categorieService.js
@@ -5,6 +5,16 @@ const addCategorieService = async (name) => {
     return { code: 201, newCategorie };
 };
 
+const getCategoriesService = async () => {    
+    const categories = await Categorie.findAll();
+
+    if (!categories) {
+        return { code: 409, message: 'Not catgories in db' };
+      }
+    return { code: 200, categories };
+};
+
 module.exports = {
     addCategorieService,
+    getCategoriesService,
 };
